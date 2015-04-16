a970564b257f92e662f84028f295a718e3b907e7
guowei <gw@guoweideMac-mini.local>
2015-04-16 11:07:17 +0800

    Initial Commit

---
+++ CollectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionDemo
+//
+//  Created by guowei on 15/4/16.
+//  Copyright (c) 2015年 guowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionDemo
+//
+//  Created by guowei on 15/4/16.
+//  Copyright (c) 2015年 guowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
